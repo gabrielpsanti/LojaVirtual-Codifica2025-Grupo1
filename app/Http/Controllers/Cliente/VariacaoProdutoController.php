22f4f9a5a13ba9f658ee28c6daca8b696f59e3bb
Rodrigo Inocente <ridrigocorreia@gmail.com>
2026-03-29 22:43:13 -0300

    Limpar controller cliente

--- app/Http/Controllers/Cliente/VariacaoProdutoController.php
+++ app/Http/Controllers/Cliente/VariacaoProdutoController.php
@@ -17,49 +17,9 @@ class VariacaoProdutoController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(VariacaoProduto $variacaoProduto)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(VariacaoProduto $variacaoProduto)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, VariacaoProduto $variacaoProduto)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(VariacaoProduto $variacaoProduto)
     {
         //
     }
